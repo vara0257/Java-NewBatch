ce939fdccf870c8470b34a3625a2af8608ec3aff
TagorenathV <tagorenath.v@gmail.com>
2018-06-25 23:36:19 +0530

    25-06-2018

---
+++ 25-06-2018/test.java
@@ -0,0 +1,16 @@
+class test
+{
+  public static void main(String[] args) {
+       int a; //declaration
+       a=1; // initialization
+       System.out.println(a);
+
+       int b = 1; // dec and init
+       System.out.println(b);
+
+
+       final int c = 3; // decal
+       c = 3;
+       System.out.println(c);
+  }
+}
