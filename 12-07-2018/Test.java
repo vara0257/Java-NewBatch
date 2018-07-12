e36e6639b46ef91fdf17dc983a70761b472ed70e
TagorenathV <tagorenath.v@gmail.com>
2018-07-13 00:20:32 +0530

    12-07-2018

---
+++ 12-07-2018/Test.java
@@ -0,0 +1,12 @@
+class Test{
+  public static void main(String[] args) {
+    final int a;// declaration
+    a = 1; // init
+
+    final int b = 10; // dec and init
+    b = 20;
+    System.out.println(a + " | " + b);
+
+
+  }
+}
